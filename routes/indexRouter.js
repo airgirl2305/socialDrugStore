cdf7d7edd0b0f5e1b7a4b8b9b5d95f589a005ccd
AirGirl2305 <kheda.askhabova@gmail.com>
2022-04-07 15:55:47 +0300

    R5

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -8,18 +8,17 @@ router.route('/')
     console.log(drugs);
     //собрать новый объект с исправленной ценой drug_id:price
       //или массив лекарство - картинка - цена
-    const drugsForPrices = await Drug.findAll({include: {
+/*     const drugsForPrices = await Drug.findAll({include: {
       model:Category,
-        //include:
+      include: discount,
       }});
     const prices =  drugs.map((el) => ({ ...el, price:(el.percent) ? el.price - (el.percent * el.percent)  : el.price})); //  проитерироваться
-    console.log("-__________________", res.locals.user);
-    
+     */
     const freeDrugs1week = [drugs[7] + drugs[3] + drugs[2]];
     const freeDrugs2week = [drugs[4] + drugs[9] + drugs[12]];
 
     
-    res.render('index', { drugs, prices  });
+    res.render('index', { drugs }); //    res.render('index', { drugs, prices  });
   });
  
  module.exports = router;
